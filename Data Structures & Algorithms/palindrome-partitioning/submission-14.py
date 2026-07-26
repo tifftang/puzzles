ce0b04b37d183497de807fdd0f0dbc0c6b06e4bf
cozyjinx <tiffanytang2014@gmail.com>
2026-07-26 14:13:44 -0700

    Add: palindrome-partitioning - submission-14

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-14.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        memo = {}
+        def isPali(l, r):
+            if (l, r) in memo: return memo[(l, r)]
+            o_l, o_r = l, r
+            while l < r:
+                if s[l] != s[r]: 
+                    memo[(o_l, o_r)] = False
+                    return False
+                l += 1
+                r -= 1
+            memo[(o_l, o_r)] = True
+            return True
+
+        def bt(i, ans):
+            if i == len(s):
+                result.append(ans)
+                return
+            
+            for j in range(i + 1, len(s) + 1):
+                if isPali(i, j - 1):
+                    bt(j, ans + [s[i:j]])
+        bt(0, [])
+        return result
