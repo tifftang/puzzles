4014f088d949afe0dab50d407a54a18bb41c0089
cozyjinx <tiffanytang2014@gmail.com>
2026-08-20 02:07:40 -0700

    Add: decode-string - submission-4

---
+++ Data Structures & Algorithms/decode-string/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr = ""
+        num = 0
+        for ch in s:
+            if ch.isnumeric():
+                num = num * 10 + int(ch)
+            elif ch == "[":
+                stack.append((num, curr))
+                num = 0
+                curr = ""
+            elif ch == "]":
+                m, prev = stack.pop()
+                curr = prev + curr * m
+            else:
+                curr += ch
+        return curr
