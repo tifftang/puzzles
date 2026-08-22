0317e77f4288e0fa15b3fdf7671d86708ed89f77
cozyjinx <tiffanytang2014@gmail.com>
2026-08-21 17:44:19 -0700

    Add: single-threaded-cpu - submission-5

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        time = []
+        avail = []
+        for idx, (enqueue, process) in enumerate(tasks):
+            heapq.heappush(time, (enqueue, process, idx))
+        t = 1
+        res = []
+        while time or avail:
+            while time and time[0][0] <= t:
+                _, process, idx = heapq.heappop(time)
+                heapq.heappush(avail, (process, idx))
+            
+            if avail:
+                process, idx = heapq.heappop(avail)
+                t += process
+                res.append(idx)
+            else:
+                t += 1
+        return res
